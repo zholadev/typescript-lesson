1c0cc777c5ad703508ad5119a0c2075d72e636cc
zholadev <zholadev.io@gmail.com>
2023-01-09 00:09:57 +0600

    ts types

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 "use strict";
-const a = 1;
+let revenue = 1000;
+let bonus = 100;
+let c = false;
+let d = true;
+let stroke = 'zholadev';
+let strike2 = 'coder';
+let res = revenue + bonus;
+console.log(res);
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-const a = 1;
+let revenue: number = 1000;
+let bonus: number = 100;
+let c: boolean = false;
+let d: boolean = true;
+let stroke: string = 'zholadev'
+let strike2: string = 'coder'
+
+let res: number = revenue + bonus
+
+console.log(res)
