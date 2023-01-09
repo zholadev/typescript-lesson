7f6eaa7e86f437e5ed98461c5443d395e831f21c
zholadev <zholadev.io@gmail.com>
2023-01-09 23:14:30 +0600

    type guard in typescript

--- module_4/app.ts
+++ module_4/app.ts
@@ -347,3 +347,35 @@ function userToAmin(user: UserS): Admin {
         role: 1
     }
 }
+
+// Type Guard
+
+function logId(id: string | number) {
+    if (isString(id)) {
+        console.log(id)
+    } else {
+        console.log(id)
+    }
+
+    id
+}
+
+function isString(x: string | number): x is string {
+    return typeof x === 'string'
+}
+
+function isAdmin(user: UserS | Admin): user is Admin {
+    return 'role' in user;
+}
+
+function isAdminAlternative(user: UserS | Admin): user is Admin {
+    return (user as Admin).role !== undefined
+}
+
+function setRole(user: UserS | Admin) {
+    if (isAdmin(user)) {
+        user.role = 0;
+    } else {
+        throw new Error('user is not admin')
+    }
+}
