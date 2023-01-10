2afcbdbc3f9dadf3bba2b489a14342918a0a4a07
zholadev <zholadev.io@gmail.com>
2023-01-10 17:11:47 +0600

    extends practice

--- class/extends.ts
+++ class/extends.ts
@@ -35,3 +35,64 @@ class PersistedPayment extends Paymeent {
         }
     }
 }
+
+class ClientS {
+    name: string = 'user'
+
+    constructor() {
+        // this.name = name
+        console.log(this.name)
+    }
+}
+
+class AdminS extends ClientS {
+    name: string = 'admin'
+
+    constructor() {
+        super();
+        console.log(this.name)
+    }
+
+}
+
+new AdminS()
+
+class ClientB {
+    name: string
+
+    constructor(name: string) {
+        this.name = name
+    }
+}
+
+class Clients extends Array<ClientB> {
+    searchByName(name: string) {
+        this.filter(user => user.name === name)
+    }
+
+    toString(): string {
+        return this.map(user => user.name).join(', ')
+    }
+}
+
+const userss = new Clients()
+userss.push(new ClientB('ghost'))
+userss.push(new ClientB('asdas'))
+
+console.log(userss.toString())
+
+class Kaz {
+    date: Date;
+}
+
+class Kaz2 {
+    user: ClientB;
+    kaz: Kaz
+
+    constructor(user: ClientB, kaz: Kaz) {
+        this.kaz = kaz
+        this.user = user
+    }
+}
+
+// const tot = new Kaz2('Shaman', new Date())
